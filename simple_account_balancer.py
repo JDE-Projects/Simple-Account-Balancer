ede0e26b7d3df91b2b304fca1949fa4a405f90fc
JDE-Projects <289071709+JDE-Projects@users.noreply.github.com>
2026-07-19 09:16:00 -0500

    Bump version to v1.7.0

--- simple_account_balancer.py
+++ simple_account_balancer.py
@@ -32,7 +32,7 @@ from decimal import Decimal, InvalidOperation, ROUND_HALF_UP
 
 import webview
 
-APP_VERSION = "1.6.0"
+APP_VERSION = "1.7.0"
 GITHUB_OWNER = "JDE-Projects"
 GITHUB_REPO = "Simple-Account-Balancer"
 
